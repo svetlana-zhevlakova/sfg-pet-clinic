8c36463976961946e3cfc889e5b09721e6ee78ef
Svetlana Zhevlakova <Svetlana_Zhevlakova@epam.com>
2018-09-11 09:04:36 +0300

    closes #33. enhanced owners with contact info and pets. updated owner save method

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/OwnerMapServiceImpl.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/OwnerMapServiceImpl.java
@@ -1,14 +1,51 @@
 package guru.springframework.sfgpetclinic.service.map;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.service.OwnerService;
+import guru.springframework.sfgpetclinic.service.PetService;
+import guru.springframework.sfgpetclinic.service.PetTypeService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OwnerMapServiceImpl extends AbstractMapService<Owner, Long> implements OwnerService {
 
+    private final PetService petService;
+    private final PetTypeService petTypeService;
+
+    public OwnerMapServiceImpl(PetService petService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.petTypeService = petTypeService;
+    }
+
     @Override
     public Owner findByLastName(String lastName) {
         return null;
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        if(object != null){
+            object.getPets().forEach(pet -> {
+                if(pet.getPetType() != null){
+                    PetType petType = pet.getPetType();
+                    if(petType.getId() == null){
+                        pet.setPetType(petTypeService.save(petType));
+                    }
+                }else{
+                    throw  new RuntimeException("Pet Type is required");
+                }
+
+                if(pet.getId() == null){
+                    Pet savedPet = petService.save(pet);
+                    pet.setId(savedPet.getId());
+                }
+            });
+
+            return super.save(object);
+        }else{
+            return null;
+        }
     }
 }
