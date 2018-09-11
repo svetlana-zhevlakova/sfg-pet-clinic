95f8f63a000056b3724569810b61c6112eaa90f7
Svetlana Zhevlakova <Svetlana_Zhevlakova@epam.com>
2018-09-11 09:13:26 +0300

    closes #31. created speciality map service impl

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/SpecialityService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinic.service;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/SpecialityMapServiceImpl.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.service.map;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.service.SpecialityService;
+
+public class SpecialityMapServiceImpl extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+}
