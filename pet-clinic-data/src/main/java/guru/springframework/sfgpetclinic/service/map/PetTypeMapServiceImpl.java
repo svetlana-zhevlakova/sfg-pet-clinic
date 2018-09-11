29775a817c8d80e512cb87fb72304951fc10a95a
Svetlana Zhevlakova <Svetlana_Zhevlakova@epam.com>
2018-09-11 08:25:06 +0300

    closes #29. created PetType map service impl

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/PetTypeMapServiceImpl.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.service.map;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.service.PetTypeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PetTypeMapServiceImpl extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+}
