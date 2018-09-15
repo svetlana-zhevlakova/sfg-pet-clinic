4acbef0982e9096fa5cb12e116afe193e2bac63a
Svetlana Zhevlakova <svettkins@gmail.com>
2018-09-15 18:14:01 +0300

    closes #37. converted vets to JPA entity

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -1,5 +1,8 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Speciality extends BaseEntity {
 
     private String description;
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,10 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 public class Vet extends Person {
 
+    @ManyToMany
+    @JoinTable(name = "vet_speciality",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
