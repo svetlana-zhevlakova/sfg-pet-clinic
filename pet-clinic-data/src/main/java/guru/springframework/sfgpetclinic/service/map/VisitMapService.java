529ea1426a6676b6e64cf8c445132bdc1befe9a5
Svetlana Zhevlakova <svettkins@gmail.com>
2018-09-17 08:34:19 +0300

    closes #46. created visit map service

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/VisitMapService.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgpetclinic.service.map;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.service.VisitService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit == null || visit.getPet() == null || visit.getPet().getId() == null
+                || visit.getPet().getOwner() == null || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid visit info");
+        }
+        return super.save(visit);
+    }
+}
